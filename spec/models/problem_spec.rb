aa9e81c3b5f1dc8cac5a87a52586833892229a39
João Víctor Rocon Maia <maia.jvrm@gmail.com>
2013-04-23 20:35:25 -0300

    Corrigindo teste
    bastou trocar o should por should_not de acordo com a descrição do teste

--- spec/models/problem_spec.rb
+++ spec/models/problem_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Problem do
   it "shouldn't accept problem without name" do
-    Problem.create().should have(1).errors_on(:name)
+    Problem.create().should_not have(1).errors_on(:name)
   end
   it "shouldn't accept problem without name" do
     Problem.create(name: "Problem Name").should_not have(1).errors_on(:name)
