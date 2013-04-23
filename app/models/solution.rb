84b9486cda82efee7b32c396324bd5fae1349dd5
João Víctor Rocon Maia <maia.jvrm@gmail.com>
2013-04-23 20:47:29 -0300

    Campo "name" obrigatório em "Problem" e "Solution"
    apenas adicionei os validates nos modelos e ajustei os testes.
    nos testes de modelo acabei mudando os testes.
    os testes do controller apenas adicionei o atributo name.

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -1,3 +1,5 @@
 class Solution < ActiveRecord::Base
   attr_accessible :name
+
+  validates :name, presence: true
 end
--- spec/models/problem_spec.rb
+++ spec/models/problem_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe Problem do
   it "shouldn't accept problem without name" do
-    Problem.create().should_not have(1).errors_on(:name)
+    Problem.create().should have(1).errors_on(:name)
   end
-  it "shouldn't accept problem without name" do
-    Problem.create(name: "Problem Name").should_not have(1).errors_on(:name)
+  it "should accept problem with name" do
+    Problem.create(name: "Problem Name").should have(0).errors_on(:name)
   end
 end
